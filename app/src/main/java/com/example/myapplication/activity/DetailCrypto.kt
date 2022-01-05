2b45b29740a4424faa031f17154e3f2ec546ae6b
yusrilalbi <aa684011@gmail.com>
2022-01-05 09:42:15 +0700

    FIX 30%

---
+++ app/src/main/java/com/example/myapplication/activity/DetailCrypto.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailCrypto : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_crypto)
+    }
+}

e51f4df4bec5d166e2bfe26a2f07d8787458e2bd
yusrilalbi <aa684011@gmail.com>
2022-01-06 09:28:46 +0700

    Fix 50%

---
+++ app/src/main/java/com/example/myapplication/activity/Tabel.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tabel : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tabel)
+    }
+}
